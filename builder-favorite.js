4f1e94974eb7f388dffeed4f2e2756f5a0208566
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2022-01-10 10:39:59 +0100

    Update

---
+++ builder-favorite.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+
+const writeFile = promisify(fs.writeFile)
+
+const articles = require('./api/favorite.json')
+
+const apiFolderPath = 'api'
+
+const rebuild = async () => {
+  await Promise.all(articles.map(c => {
+    const filepath = path.join(__dirname, apiFolderPath, 'id-favorite', `${c.id}.json`)
+    return writeFile(filepath, JSON.stringify(c, null, 2))
+  }))
+  .catch(console.error)
+
+  console.log('build successful')
+}
+
+rebuild()
